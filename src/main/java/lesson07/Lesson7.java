94d96080ca8dbe3e36aee1422d98ce05326f560a
Vitalii Luzhnov <vitalii@luzhnov.pro>
2022-02-02 12:15:38 +0300

    Правки к ДЗ по 6 уроку + урок 7 /02.02.2022/

---
+++ src/main/java/lesson07/Lesson7.java
@@ -0,0 +1,10 @@
+package lesson07;
+
+class Lesson7 {
+    public static void main(String[] args){
+        int k = 0;
+        String s = "hello";
+        s += ", world";
+        System.out.println(s);
+    }
+}
