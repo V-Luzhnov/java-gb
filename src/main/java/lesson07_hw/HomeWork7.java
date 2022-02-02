00e49faafa400f18d137684defbc681d408b1666
Vitalii Luzhnov <vitalii@luzhnov.pro>
2022-02-02 13:57:51 +0300

    ДЗ 7 - начало /02.02.2022/

--- src/main/java/lesson07_hw/HomeWork7.java
+++ src/main/java/lesson07_hw/HomeWork7.java
@@ -1,42 +1,37 @@
-package lesson07;
-
-class Lesson7 {
+package lesson07_hw;
+/**
+ * Java 1. HomeWork 7
+ *
+ * @author Vitalii Luzhnov
+ * @version 02.02.2022
+ */
+class HomeWork7 {
     public static void main(String[] args) {
         Cat cat = new Cat("Barsik", 5);
         Plate plate = new Plate(10);
         System.out.println(cat);
         System.out.println(plate);
 
-//-1-       plate.setFood(plate.getFood() - cat.getAppetite());
-//-2-       plate.decreaseFood(cat.getAppetite());
         cat.eat(plate);
 
         System.out.println(plate);
     }
 }
 
-//-3+I- interface ICat {
-//-3+I-     void eat(Plate plate);
-//-3+I- }
-
 class Cat {
-//-3+I- class Cat implements ICat {
     private String name;
     private int appetite;
+    private boolean isFull;
 
     Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
+        isFull = false;
     }
 
-//-3+I-    @Override
     public void eat(Plate plate) {
         plate.decreaseFood(appetite);
     }
-
-//-2-    public int getAppetite() {
-//-2-        return appetite;
-//-2-    }
 
     @Override
     public String toString() {
@@ -50,14 +45,6 @@ class Plate {
     Plate(int food) {
         this.food = food;
     }
-
-//-1-    public int getFood() {
-//-1-        return food;
-//-1-    }
-//-1-
-//-1-    public void setFood(int food) {
-//-1-        this.food = food;
-//-1-    }
 
     public void decreaseFood(int food) {
         this.food -= food;
