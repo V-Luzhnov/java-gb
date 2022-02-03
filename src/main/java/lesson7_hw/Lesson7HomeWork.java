0dd919ac675b5e4b7957745884b4cb0a67b4bb58
Vitalii Luzhnov <vitalii@luzhnov.pro>
2022-02-03 18:13:57 +0300

    ДЗ 7 - финальная версия /03.02.2022/

--- src/main/java/lesson7_hw/Lesson7HomeWork.java
+++ src/main/java/lesson7_hw/Lesson7HomeWork.java
@@ -2,43 +2,63 @@ package lesson7_hw;
 
 public class Lesson7HomeWork {
     public static void main(String[] args) {
+
+        int foodAdd = 35; // количество рыбы для добавления
         int food = 30; // начальное количество рыбы в тарелке
-        int foodAdd = 40; // количество рыбы для добавления
         final int sizePlate = 30; // объем тарелки, т.е. сколько рыбы помещается в тарелку
+
+        final String ANSI_RESET = "\033[0m";
+        final String ANSI_GREEN = "\033[32m";
+        final String ERROR_1 = "Начальное количество рыбы не умещается в тарелке. Уменьшите начальное количество рыбы в тарелке.";
+        final String ERROR_2 = "Тарелка слишком маленькая. Возьмите тарелку побольше.";
+        final String Q_IN_PLATE = "Количество рыбы в тарелке: ";
+        final String Q_FOODADD = "Количество рыбы в запасе: ";
 
         Cat[] cats = {
                 new Cat("Barsik", 10),
-                new Cat("Murzik", 30),
-                new Cat("Kotik", 20)
+                new Cat("Murzik", 20),
+                new Cat("Kotik", 30),
+                new Cat("Leopold", 15)
         };
 
-        if (!checkSizePlate(sizePlate, cats)) {
-            System.out.println("Тарелка слишком маленькая. Возьмите тарелку побольше.");
+        if (sizePlate < food) {
+            System.out.println(ERROR_1);
             return;
         }
 
-        System.out.println("Запас рыбы: " + foodAdd);
+        if (!checkSizePlate(sizePlate, cats)) {
+            System.out.println(ERROR_2);
+            return;
+        }
+
+        System.out.println("Количество рыбы, которое помещается в тарелку: " + sizePlate + "\n" + Q_FOODADD + foodAdd);
 
         Plate plate = new Plate(food, sizePlate);
-        System.out.println("Рыб в тарелке до еды: " + plate);
 
-        for (Cat cat : cats) {
-            cat.eat(plate);
-            System.out.println(cat);
-        }
+        System.out.println(Q_IN_PLATE + plate);
 
-        System.out.println("Рыб в тарелке после еды: " + plate);
-
-        // добавляем еду, если есть голодный кот
-        if (catHungry(cats)) {
-            foodAdd = plate.addFood(foodAdd);
-            System.out.println("foodAdd 2: " + foodAdd);
+        // кормим котиков пока есть еда в запасе
+        while (catHungry(cats) && foodAdd != 0) {
             for (Cat cat : cats) {
                 cat.eat(plate);
-                System.out.println(cat);
+                System.out.println(ANSI_GREEN + cat + ANSI_RESET);
             }
+            System.out.println(Q_IN_PLATE + plate);
+            // если после еды есть голодные котики, то добавляем еды в тарелку
+            if (catHungry(cats)) {
+                foodAdd = plate.addFood(foodAdd);
+            }
+            System.out.println(Q_FOODADD + foodAdd);
         }
-        System.out.println("Рыб в тарелке после еды: " + plate);
+
+        // доесть то, что осталось в тарелке, если будет найден голодный котик с подходящим аппетитом
+        if (catWithLittleAppetite(plate.getFood(), cats)) {
+            for (Cat cat : cats) {
+                cat.eat(plate);
+                System.out.println(ANSI_GREEN + cat + ANSI_RESET);
+            }
+            System.out.println(Q_IN_PLATE + plate + "\n" + Q_FOODADD + foodAdd);
+        }
     }
 
     //проверка на наличие голодного кота
@@ -51,6 +71,7 @@ public class Lesson7HomeWork {
         return false;
     }
 
+    // проверка соответствия размера тарелки аппетиту каждого кота
     static boolean checkSizePlate(int sizePlate, Cat[] cats) {
         for (Cat cat : cats) {
             if (sizePlate < cat.getAppetite()) {
@@ -58,6 +79,16 @@ public class Lesson7HomeWork {
             }
         }
         return true;
+    }
+
+    // поиск голодного кота, аппетита которого удовлетворил бы остаток еды в тарелке
+    static boolean catWithLittleAppetite(int foodInPlate, Cat[] cats) {
+        for (Cat cat : cats) {
+            if (foodInPlate >= cat.getAppetite() && !cat.getIsFull()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
@@ -102,6 +133,10 @@ class Plate {
         this.sizePlate = sizePlate;
     }
 
+    public int getFood() {
+        return food;
+    }
+
     public boolean decreaseFood(int food) {
         if ((this.food - food) < 0) {
             return false;
@@ -112,36 +147,20 @@ class Plate {
 
     // Метод добавления еды в тарелку
     public int addFood(int foodAdd) {
-        System.out.println("this.food: " + this.food);
-        System.out.println("foodAdd: " + foodAdd);
-        System.out.println("this.sizePlate: " + this.sizePlate);
+        final String Q_AFTER_ADD = "Количество рыбы в тарелке: ";
+        final String Q_ADD = "Добавлено рыбы: ";
+
         if (this.food + foodAdd <= this.sizePlate) {
-            System.out.println("Добавлено: " + foodAdd);
+            System.out.println(Q_ADD + foodAdd);
             this.food += foodAdd;
-            System.out.println("Рыб в тарелке после добавления: " + this.food);
+            System.out.println(Q_AFTER_ADD + this.food);
             return 0;
-        } else {
-            this.food = this.sizePlate;
-            System.out.println("Рыб в тарелке после добавления: " + this.food);
-            return foodAdd - this.sizePlate;
         }
-//        System.out.println("Рыб в тарелке после добавления: " + this.food);
-
-//    public void addFood(int foodAdd) {
-//        System.out.println("this.food: " + this.food);
-//        System.out.println("foodAdd: " + foodAdd);
-//        System.out.println("this.sizePlate: " + this.sizePlate);
-//        if (this.food + foodAdd <= this.sizePlate) {
-//            System.out.println("Добавлено: " + foodAdd);
-//            this.food += foodAdd;
-//        } else {
-//            this.food = this.sizePlate;
-//        }
-//        System.out.println("Рыб в тарелке после добавления: " + this.food);
-
-//        System.out.println("Добавлено: " + foodAdd);
-//        this.food += foodAdd;
-//        System.out.println("Рыб в тарелке после добавления: " + this.food);
+        int newFoodAdd = foodAdd - (this.sizePlate - this.food);
+        System.out.println(Q_ADD + (this.sizePlate - this.food));
+        this.food = this.sizePlate;
+        System.out.println(Q_AFTER_ADD + this.food);
+        return newFoodAdd;
     }
 
     @Override
